61174852bd9e6f8ef0cdc5314ee97fdf88af037d
kamran/faiz <faizkamran961@gmail.com>
2026-05-16 16:17:34 +0500

    add clients controller with store, update, destroy

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -14,4 +14,44 @@ class ClientController extends Controller
             'clients' => $clients,
         ]);
     }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'business_id' => 'exists:businesses,id',
+            'name' => 'required|string|max:100',
+            'email' => 'nullable|email',
+            'phone' => 'nullable|string',
+            'city' => 'string|required',
+            'ntn' => 'string|nullable',
+            'cnic' => 'string|nullable',
+            'client_type' => 'required|in:b2b,b2c',
+            'address' => 'string|required',
+
+
+        ]);
+        Client::create($validated);
+        return redirect()->back()->with('Success', 'Client Created Successfully');
+    }
+
+    public function update(Request $request, Client $client){
+        $validated = $request->validate([
+         'business_id' => 'exists:businesses,id',
+            'name' => 'required|string|max:100',
+            'email' => 'nullable|email',
+            'phone' => 'nullable|string',
+            'city' => 'string|required',
+            'ntn' => 'string|nullable',
+            'cnic' => 'string|nullable',
+            'client_type' => 'required|in:b2b,b2c',
+            'address' => 'string|required',
+    
+        ]);
+        $client->update($validated);
+        return redirect()->back()->with('Success', 'Client Updated Successfully');
+    }
+
+    public function destroy(Client $client){
+        $client->delete();
+        return redirect()->back()->with('Success', 'Client Deleted Successfully');
+    }
 }
