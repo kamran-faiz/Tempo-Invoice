f6a64da433c232d29c7497ecc4667cd467b46359
kamran/faiz <faizkamran961@gmail.com>
2026-05-16 23:00:31 +0500

    fix custom font size classes for Tailwind v4

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,9 +9,9 @@
     @inertiaHead
     @routes
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"/>
-<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=block" rel="stylesheet"/>
 </head>
-<body>
+<body class="bg-background text-on-background font-sans antialiased">
     @inertia
 </body>
 </html>
